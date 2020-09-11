b85b8ac153fed84cc243d660f8811462a8fe82c1
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-09-11 16:07:04 -0700

    deleted code for sending measurements

--- main.py
+++ main.py
@@ -30,31 +30,12 @@ def make_app():
     return tornado.web.Application(handlers, debug=True)
 
 
-def send_measurement():
-    try:
-        request_fields = {}
-        request_fields["timestamp"] = time.time()
-        request_fields["values"] = sensor.measurements
-        r = requests.post(
-            config["uri"] + "/nodes/" + config["node_id"] + "/measurements/",
-            headers={"Content-Type": "application/json"},
-            json=request_fields,
-            timeout=2,
-        )
-        print(r.status_code)
-    finally:
-        tornado.ioloop.IOLoop.instance().add_timeout(
-            timedelta(seconds=1), send_measurement
-        )
-
-
 if __name__ == "__main__":
 
     with open("/home/pi/smokey/config.json", "r") as f:
         config = json.loads(f.read())
 
     with Pms7003Thread(config["serial_port"]) as sensor:
-        # send_measurement()
         app = make_app()
         app.listen(8888)
         tornado.ioloop.IOLoop.current().start()
